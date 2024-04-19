8e15fb11b0c108753e86eeb54a4adf0af2a6fb30
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2024-04-19 15:06:26 +0800

    Update register.js

--- register.js
+++ register.js
@@ -5,7 +5,7 @@ var Register = {
     checkIfEmailExists: function (email) {
         // check from Pantry the email address
         let exists = false;
-        Register.getPantryData('https://getpantry.cloud/apiv1/pantry/8c1037f6-bf4b-443d-9941-a9f9c6a99671/basket/users', function (data) {
+		if(Register.validateEmail(email)) Register.getPantryData('https://getpantry.cloud/apiv1/pantry/8c1037f6-bf4b-443d-9941-a9f9c6a99671/basket/users', function (data) {
             data = JSON.parse(data);
             let keys = Object.keys(data);
             for (i = 0; i < keys.length; i++) {
@@ -18,13 +18,15 @@ var Register = {
         return exists;
     },
 
-    createAccount: function () {
+    createAccount: function (email) {
         // check first if the email exists
-
+        if (!Register.checkIfEmailExists(email)) {}
+        else
+            window.alert('Your email address is not existing!');
     },
 
     verifySameEmail: function () {},
-	
+
     verifySamePassword: function () {},
 
     getBlobRecord: function (url, callback) {
@@ -97,5 +99,31 @@ var Register = {
         req.open('GET', url, false);
         req.setRequestHeader('Content-Type', 'application/json');
         req.send();
+    },
+
+    createPantryData: function (data, url, callback) {
+        let req = new XMLHttpRequest();
+
+        req.onload = () => {
+            if (req.readyState == 4)
+                if (req.status == 200)
+                    if (callback)
+                        callback(req.response);
+        }
+
+        req.onerror = (err) => {
+            window.alert('Error encountered! ' + err);
+        }
+
+        req.open('GET', url, false);
+        req.setRequestHeader('Content-Type', 'application/json');
+        req.send(data);
+    },
+
+    validateEmail: function(email) {
+        return String(email)
+        .toLowerCase()
+        .match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
     }
 }
