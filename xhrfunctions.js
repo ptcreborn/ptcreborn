65ee582651a8b80e2914d0da16f445ee5488266f
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2024-04-30 13:07:24 +0800

    Update xhrfunctions.js

--- xhrfunctions.js
+++ xhrfunctions.js
@@ -1,42 +1,4 @@
 var XHRFunctions = {
-  createAccount: function (email, blobData, elem, succesRedirectUrl) {
-        // check first if the email exists
-        let emailExists = Register.checkIfEmailExists(email);
-      	console.log('done checking emails');
-        if (emailExists == 0) {
-            /* blobData Structure = {
-            "nickname": "value",
-            "joined": "value",
-            "prof_image": "value",
-            "background_image": "value",
-            "about_me": "value"
-            };*/
-
-            console.log('email not existing..');
-
-            Register.createRecordBlob(blobData, function (data) {
-                console.log('creating blob record');
-                // data is the id of the blob created.
-                // then create a pantryData
-                let pantryData = JSON.stringify({
-                    [email]: {
-                        "blob_id": data.split('jsonBlob/')[1]
-                    }
-                });
-                Register.createPantryData(pantryData, 'https://getpantry.cloud/apiv1/pantry/8c1037f6-bf4b-443d-9941-a9f9c6a99671/basket/users', function (data) {
-                    elem.innerText = 'Your account is successfully created!';
-                    console.log('pantry record created.');
-                    setTimeout(function () {
-                        window.location.href = succesRedirectUrl;
-                    }, 1000);
-                });
-            });
-        } else if (emailExists == 1) {
-            elem.innerText = 'The email address is already registered, try other email address.';
-        } else if (emailExists == 2) {
-            elem.innerText = 'You have input invalid email address.';
-        }
-    },
 
     getBlobRecord: function (url, callback) {
         let req = new XMLHttpRequest();
