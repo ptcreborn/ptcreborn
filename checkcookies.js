791186c6bd4d35f50bfad7995fc5d4392448bd52
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2024-04-30 13:24:12 +0800

    Create checkcookies.js

---
+++ checkcookies.js
@@ -0,0 +1,39 @@
+
+var PTC_Cookies = {
+
+    checkIfCookiesSupported: function () {
+        return navigator.cookieEnabled;
+    },
+
+    retrieveCookies: function(path) {
+        if (PTC_Cookies.getCookieName(path))
+            return JSON.parse(PTC_Cookies.getCookieName(path));
+        else 
+			return null;		
+    },
+	
+	storeCookies: function(path, jsonStr, timeInMs) {
+		PTC_Cookies.setCookie(path, jsonStr, timeInMs);
+	},
+
+    setCookie: function (e, t, l) {
+        var o = new Date;
+        o = new Date(o.getTime() + 1e3 * l),
+        document.cookie = e + "=" + t + "; expires=" + o.toGMTString() + ";";
+    },
+
+    getCookieName: function(e) {
+        var t = document.cookie,
+        l = e + "=",
+        o = t.indexOf("; " + l);
+        if (-1 == o) {
+            if (0 != (o = t.indexOf(l)))
+                return null
+        } else {
+            o += 2;
+            var n = document.cookie.indexOf(";", o);
+             - 1 == n && (n = t.length)
+        }
+        return decodeURI(t.substring(o + l.length, n))
+    }
+}
