1d85d8f6e1165da434b638df45731544a774a57e
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2024-05-01 13:45:36 +0800

    Create jsonblobfunctions.js

---
+++ jsonblobfunctions.js
@@ -0,0 +1,60 @@
+var JBLOBFunctions = {
+
+    getBlobRecord: function (url, callback) {
+        let req = new XMLHttpRequest();
+
+        req.onload = () => {
+            if (req.readyState == 4)
+                if (req.status == 200)
+                    if (callback)
+                        callback(req.response);
+                    else if (req.status == 404)
+                        callback('404');
+        }
+
+        req.onerror = (err) => {
+            window.alert('Error encountered! ' + err);
+        }
+
+        req.open('GET', url, false);
+        req.setRequestHeader('Content-Type', 'application/json');
+        req.send();
+    },
+
+    createRecordBlob: function (data, callback) {
+
+        let xhr = new XMLHttpRequest();
+        xhr.withCredentials = true;
+        xhr.open('POST', 'https://jsonblob.com/api/jsonBlob', false);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.setRequestHeader('Accept', 'application/json');
+
+        xhr.onload = function () {
+            console.log(xhr.getAllResponseHeaders().split('location: ')[1]);
+            callback(xhr.getAllResponseHeaders().split('location: ')[1]);
+        };
+
+        xhr.send(data);
+    },
+	
+	updateRecordBlob: function(data, url, callback) {
+		let req = new XMLHttpRequest();
+
+        req.onload = () => {
+            if (req.readyState == 4)
+                if (req.status == 200)
+                    if (callback)
+                        callback(req.response);
+                    else if (req.status == 404)
+                        callback('404');
+        }
+
+        req.onerror = (err) => {
+            window.alert('Error encountered! ' + err);
+        }
+
+        req.open('PUT', url, false);
+        req.setRequestHeader('Content-Type', 'application/json');
+        req.send(JSON.stringify(data));
+	}
+}
