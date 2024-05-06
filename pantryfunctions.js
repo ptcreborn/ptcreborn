c394983382b8783c0bec85c47c6943e194cbcedf
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2024-05-06 19:10:19 +0800

    Create pantryfunctions.js

---
+++ pantryfunctions.js
@@ -0,0 +1,38 @@
+var Pantry = {
+  getPantryData: function (url, callback) {
+        let req = new XMLHttpRequest();
+
+        req.onload = () => {
+            if (req.readyState == 4)
+                if (req.status == 200)
+                    return callback(req.response);
+        }
+
+        req.onerror = (err) => {
+            window.alert('Error encountered! ' + err);
+        }
+
+        req.open('GET', url, false);
+        req.setRequestHeader('Content-Type', 'application/json');
+        req.send();
+    },
+
+    createPantryData: function (data, url, callback) {
+        let req = new XMLHttpRequest();
+
+        req.onload = () => {
+            if (req.readyState == 4)
+                if (req.status == 200)
+                    if (callback)
+                        callback(req.response);
+        }
+
+        req.onerror = (err) => {
+            window.alert('Error encountered! ' + err);
+        }
+
+        req.open('PUT', url, false);
+        req.setRequestHeader('Content-Type', 'application/json');
+        req.send(data);
+    }
+}
